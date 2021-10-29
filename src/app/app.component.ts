9aaa4e443c8227455c6272d13d5289844b405459
cataChavez <catherin.chavez@zentagroup.com>
2021-10-29 10:10:56 -0300

    pipes from uppercase until json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = 'Angular';
+
+  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI: number = Math.PI;
+
+  percent: number = 0.234;
+
+  salary: number = 1234.5;
+
+  heroe = 
+  {
+    name: 'logan',
+    keyName: 'Wolverine',
+    age: 500,
+    adress:{
+      street: 'first',
+      number:20
+    }
+  }
 }
